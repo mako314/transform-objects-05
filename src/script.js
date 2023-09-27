056535c167f550da15840bd40d81583b6558b6be
Macolister <bispo.mac@gmail.com>
2023-09-26 23:41:14 -0400

    add cubes to group, scale rotate position group

--- src/script.js
+++ src/script.js
@@ -9,23 +9,33 @@ const scene = new THREE.Scene()
 /**
  * Objects
  */
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
-const mesh = new THREE.Mesh(geometry, material)
-scene.add(mesh)
 
-// Position 
-// mesh.position.x = 0.7
-// mesh.position.y = -0.6
-// mesh.position.z = 1
-mesh.position.set(0.7, -0.6, 1)
+const group = new THREE.Group()
+group.position.y = 1
+group.scale.y = 2
+group.rotation.y = 1
+scene.add(group)
 
-// Scale
-// mesh.scale.x = 1
-// mesh.scale.y = 1
-// mesh.scale.z = 1
-mesh.scale.set(2, 0.5, 0.5)
+const cube1 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({color : 0xff0000})
+)
 
+group.add(cube1)
+
+const cube2 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({color : 0x000ff0})
+)
+cube2.position.x = -2
+group.add(cube2)
+
+const cube3 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({color : 0x00ff000})
+)
+cube3.position.x = 2
+group.add(cube3)
 
 // Rotation
 // Half rotations are done with PI, can do it like the number, or do Math.PI. For a full rotation you can do 2 * Math.PI
@@ -33,9 +43,9 @@ mesh.scale.set(2, 0.5, 0.5)
 // You can get GIMBAL LOCK if you do rotations strangely and an axis is not working
 // Mess around with ordering 
 // You can use the re-order method below to get it rotating the way you want first
-mesh.rotation.reorder('YXZ')
-mesh.rotation.x = Math.PI * 0.25
-mesh.rotation.y = Math.PI * 0.25
+// mesh.rotation.reorder('YXZ')
+// mesh.rotation.x = Math.PI * 0.25
+// mesh.rotation.y = Math.PI * 0.25
 
 
 // Axes helper
@@ -58,7 +68,6 @@ camera.position.z = 3
 scene.add(camera)
 
 //The lookAt takes a vector three, so while you can't do just mesh, mesh.position is a vector3
-camera.lookAt(mesh.position)
 
 //Normalize will take vector length and reduce it so it is 1
 // mesh.position.normalize()
